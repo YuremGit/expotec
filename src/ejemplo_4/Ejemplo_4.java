0a069c4037e5568f8ebee127d85c4793a58e74c7
Angello Monroy <progra@gmail.com>
2024-01-19 11:17:35 -0600

    primer repositorio

---
+++ src/ejemplo_4/Ejemplo_4.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplo_4;
+//Angello Monroy
+
+public class Ejemplo_4 {
+
+    public static void main(String[] args) 
+    {
+        System.out.println("Nombre completo:");
+        System.out.println("Angello");
+        System.out.println("Yurem");
+        System.out.println("Monroy");
+        System.out.println("Elias");
+        System.out.println("Edad: 16 años");
+        System.out.println("Tel: 1234-5678");
+        System.out.println("Dirección: 7Av. zona 5 misco col 1ro. Julio");
+        System.out.println("Grado: 5to. Computación");
+        System.out.println("Comida fav: Pollo en crema");
+        System.out.println("Genero: Maculino");
+        System.out.println("Pasatiempos:");
+        System.out.println("Ir al Gym");
+        System.out.println("Jugar VideoJuegos");
+        System.out.println("Jugar Bascket ball");
+        System.out.println("Escuchar música");
+        
+    }
+    
+}
